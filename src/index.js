e20cdc471a93fed6ed8066682e09744032d761a2
deveripon <developershahadathussainripon@gmail.com>
2023-07-09 02:37:36 +0600

    to do list design update and data send to localstorage

--- src/index.js
+++ src/index.js
@@ -1,115 +1,113 @@
+//let get the task add form data
 const taskForm = document.getElementById("task-form");
-const textArea = taskForm.querySelector("textarea.task-content");
+const textArea = taskForm.querySelector(".task-content");
 const checkBox = taskForm.querySelector("input[type=checkbox]");
-const submitButton = taskForm.querySelector("input[type=submit]");
-const massageBox = document.querySelector(".massage-box");
+const alertBox = document.querySelector(".massage-box");
 const taskContainer = document.querySelector(".task-container");
-let checkValue;
 
-//lets Cread data and  send data to local storage
+//check the checkbox value
+const checkBoxValue = () => {
+  let value;
+  return checkBox.checked ? (value = true) : (value = false);
+};
+
+//Add tasks
 taskForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (textArea.value.length > 0) {
-    //lets check the checkbox value
-    if (checkBox.checked) {
-      checkValue = true;
-    } else {
-      checkValue = false;
-    }
-    //check the database is task available or not
+  //lets validate the form
+  if (!textArea.value) {
+    alertBox.innerHTML = `
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Sorry !</strong> You don't write any task description
+    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+  </div>
+    `;
+  } else {
+    //check database has data or not
     let taskData;
     getData("tasks") ? (taskData = getData("tasks")) : (taskData = []);
+    //data push to the array
     taskData.push({
-      task_id: taskData.length + 1,
+      id: taskData.length + 1,
       task_content: textArea.value,
-      isImportant: checkValue,
+      isImportant: checkBoxValue(),
       isTrashed: false,
       isCompleted: false,
     });
+    //send ddta to local storage
     sendData("tasks", taskData);
-    massageBox.innerHTML = `
-    <div class="alert alert-success alert-dismissible fade show"
-    role="alert">
-    Task Added Done !
-    <button type="button"
-      class="btn-close"
-      data-dismiss="alert"
-      aria-label="Close"></button>
-  </div>
-    `;
+    //lets reset the form value
     textArea.value = "";
     checkBox.checked = false;
-    ShowData();
-  } else {
-    massageBox.innerHTML = `
-    <div class="alert alert-danger alert-dismissible fade show"
-    role="alert">
-    <strong>Sorry !</strong> You don't write any task description
-    <button type="button"
-      class="btn-close"
-      data-dismiss="alert"
-      aria-label="Close"></button>
+    showData();
+
+    alertBox.innerHTML = `
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    Task Added Done !
+    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
   </div>
     `;
   }
 });
 
-//lets Read the data from LocalStorage and show to frontend
-function ShowData() {
-  let dbData = getData("tasks");
-  let taskList = "";
-  dbData.map((task, i) => {
-    taskList +=
-      task.isTrashed === true
-        ? ""
-        : `<li class="task">
-    <div class="row">
+//lets get the task data and show  to the frontend
+function showData() {
+  let allTask = getData("tasks");
 
-      <div class="col-sm-1 checkbox-container"
-        style="vertical-align: middle; text-align: center; margin: auto;">
+  if (!allTask) {
+    taskContainer.innerHTML = `
+    <li>
+         <p class="task-desc alert mt-5 alert-warning" style="vertical-align: middle;
+         text-align: left;
+         margin: auto;">
+          No Task Added</p>
+   </li>
+    `;
+  }
+  {
+    let singleTask = "";
 
-        <div class="complete-check"
-          data-toggle="tooltip"
-          data-placement="top"
-          title="Mark as Complete"
-          style="margin-right: 05px;">
-          <i class="fa-regular fa-circle"
-            style="color: #0c7a0b;"></i>
-
-        </div>
-
-        <div class="delete-check"
-          data-toggle="tooltip"
-          data-placement="top"
-          title="Delete task">
-          <i class="fa-solid fa-circle-xmark"></i>
-        </div>
-      </div>
-
-      <div class="col-sm-9">
-        <p class="task-desc"
-          style="vertical-align: middle;
-        text-align: left;
-        margin: auto;">${task.task_content}</p>
-      </div>
-
-      <div class="col-sm-2 priority-container"
-        style="vertical-align: middle;
-      text-align: center;
-      margin: auto;">
-       ${
-         task.isImportant === true
-           ? ` <span class="priority"><i class="fa-solid fa-star"
-       style="color: #cf9904"></i></span>`
-           : ""
-       }
-      </div>
-    </div>
-  </li>`;
-  });
-  taskContainer.innerHTML = taskList;
+    allTask.map((task, index) => {
+      singleTask += `    
+      <li class="task">
+     <div class="row">
+ 
+       <div class="col-sm-1 checkbox-container"
+         style="vertical-align: middle; text-align: center; margin: auto;">
+ 
+         <div class="complete-check" data-toggle="tooltip" data-placement="top" title="Mark as Complete"
+           style="margin-right: 05px;">
+           <i class="fa-regular fa-circle" style="color: #0c7a0b;"></i>
+ 
+         </div>
+ 
+         <div class="delete-check" data-toggle="tooltip" data-placement="top" title="Delete task">
+           <i class="fa-solid fa-circle-xmark"></i>
+         </div>
+       </div>
+ 
+       <div class="col-sm-9">
+         <p class="task-desc" style="vertical-align: middle;
+         text-align: left;
+         margin:auto 20px;">
+           ${task.task_content}</p>
+       </div>
+ 
+       <div class="col-sm-2 priority-container" style="vertical-align: middle;
+       text-align: center;
+       margin: auto;">
+ 
+     ${
+       task.isImportant === true
+         ? `    <span class="priority"><i class="fa-solid fa-star" style="color: #cf9904"></i></span>`
+         : ""
+     }
+ 
+       </div>
+     </div>
+   </li>`;
+    });
+    taskContainer.innerHTML = singleTask;
+  }
 }
-
-ShowData();
-
-//lets DELETE and Mark Complete data
+showData();
